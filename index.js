ed70794c41d4bbf8c1eae7dad97bcf620314bb94
chirchirmercy <cmercychptoo.com>
2023-05-24 16:51:32 +0300

    changes

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ document.getElementById('body').style.backgroundColor='silver';
 document.getElementById('title').style.color='green';
 
 
-let fruit=document.getElementById("container").fruits;
-console.log({fruit});
+document.getElementById('v1').style.textTransform='upperCase'
 
-
+document.getElementById('v2').style.textTransform='upperCase'
 
 let fruList=document.createElement('fruList');
-list.innerHTML= "kiwi","passion";
-document.getElementById('list').appendChild(fruList);
+fruList.innerHTML= "passion";
+document.getElementById('fruList').appendChild(fruList);
+
+let vegList=document.createElement('vegList');
+vegList.innerHTML= "spinach";
+document.getElementById('vegList').appendChild(vegList);
 
