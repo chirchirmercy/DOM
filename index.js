06f9ed70680a2dca7b8964fe6481a60efffaa682
chirchirmercy <cmercychptoo.com>
2023-05-24 16:27:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+document.getElementById('body').style.backgroundColor='silver';
+
+document.getElementById('title').style.color='green';
+
+
+let fruit=document.getElementById("container").fruits;
+console.log({fruit});
+
+
+
+let fruList=document.createElement('fruList');
+list.innerHTML= "kiwi","passion";
+document.getElementById('list').appendChild(fruList);
+
